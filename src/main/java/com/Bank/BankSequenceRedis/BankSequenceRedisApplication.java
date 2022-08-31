2425cdfede78e6f7c72cb9c05e3592fd15a13ed6
Gabriel <gabriel.sh315@gmail.com>
2022-08-31 01:49:59 -0500

    Add Eureka and Config yml

--- src/main/java/com/Bank/BankSequenceRedis/BankSequenceRedisApplication.java
+++ src/main/java/com/Bank/BankSequenceRedis/BankSequenceRedisApplication.java
@@ -2,7 +2,9 @@ package com.Bank.BankSequenceRedis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BankSequenceRedisApplication {
 
